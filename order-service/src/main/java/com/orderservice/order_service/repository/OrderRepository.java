b512c1d9f6ae3fca118d4bbd918189b82d40a4ba
Sajad Ali <34246449+alisajadg191@users.noreply.github.com>
2026-07-08 01:14:48 +0100

    updated project and organised everything. added service-discovery as well

--- order-service/src/main/java/com/orderservice/order_service/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.orderservice.order_service.repository;
-
-import com.orderservice.order_service.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
---
+++ product-service/src/main/java/com.productservice/dto/ProductResponse.java
@@ -0,0 +1,17 @@
+package com.productservice.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
